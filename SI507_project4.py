9c4af739f8e4d23d914e63f52ac5878f1a4b8365
susomi_notebook <whymeiee789@gmail.com>
2019-03-31 15:56:35 -0400

    added comments for planing project.

---
+++ SI507_project4.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import csv
+
+#
+
+CACHE_FNAME = "national_sites_cache.json"
+
+#Code for scraping data from national websites
+##NAME of site
+##TYPE of site
+##DESCRIPTION of site
+##LOCATION (in any form Richmond; Richmond, VA; VA)
+##CHALLENGE standarize state names
+
+
+#save data in CSV file
+##1 row = 1 national site
+
+#Code for caching data on json file
